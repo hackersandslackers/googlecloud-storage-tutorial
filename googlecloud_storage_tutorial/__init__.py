68692b4aa7a243d8a6abf4818ae109180fdd3911
Todd Birchard <toddbirchard@gmail.com>
2024-08-07 23:15:44 -0400

    Update __init__.py

--- googlecloud_storage_tutorial/__init__.py
+++ googlecloud_storage_tutorial/__init__.py
@@ -1,5 +1,4 @@
-"""Initialize script demonstration"""
-
+"""Initialize script."""
 from faker import Faker
 
 from config import BUCKET_DIR, BUCKET_NAME, LOCAL_DIR
@@ -11,14 +10,15 @@ from googlecloud_storage_tutorial.storage import (
     upload_files,
 )
 
+# Using the `faker` library to generate random strings
 fake = Faker()
 
 
 def init_script():
-    """Initialize script demonstration."""
+    """Execute actions on a bucket containing files."""
     print(upload_files(BUCKET_NAME, BUCKET_DIR, LOCAL_DIR))
     print(list_files())
     print(download_random_file(LOCAL_DIR))
     print(rename_file(fake.unique.first_name()))
     print(delete_file(BUCKET_NAME))
-    upload_files(BUCKET_NAME, BUCKET_DIR, LOCAL_DIR)
+    print(upload_files(BUCKET_NAME, BUCKET_DIR, LOCAL_DIR))
